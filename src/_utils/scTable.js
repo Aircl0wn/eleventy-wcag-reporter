4a4348624740c6986a174c02359ef6cfff2842ac
Ronny Hendriks <ronnyhendriks@gmail.com>
2022-01-30 23:50:18 +0100

    dev: Adjust results table

--- src/_utils/scTable.js
+++ src/_utils/scTable.js
@@ -19,37 +19,50 @@ function scTable(allIssues, language, targetLevel, targetWcagVersion) {
     (totals.operable - operable.length) +
     (totals.understandable - understandable.length) + 
     (totals.robust - robust.length);
+
+  let totalNotConforming = 
+    (perceivable.length) + 
+    (operable.length) +
+    (understandable.length) + 
+    (robust.length);
   return `
-  <table class="sc-table">
-  <thead>
+    <table class="sc-table">
+    <thead>
+      <tr>
+        <th>${translations["principle"][language]}</th>
+        <th>${translations["passed"][language]}</th>
+        <th>${translations["failed"][language]}</th>
+      </tr>
+    </thead>
+    <tbody>
     <tr>
-      <td>${translations["principle"][language]}</td>
-      <td>${translations["results_per_principle"][language]}</td>
+      <td>${translations["perceivable"][language]}</td>
+      <td>${totals.perceivable - perceivable.length} ${translations["of"][language]} ${totals.perceivable}</td>
+      <td>${perceivable.length} ${translations["of"][language]} ${totals.perceivable}</td>
     </tr>
-  </thead>
-  <tbody>
-  <tr>
-    <td>${translations["perceivable"][language]}</td>
-    <td>${totals.perceivable - perceivable.length} ${translations["of"][language]} ${totals.perceivable}</td>
-  </tr>
-  <tr>
-      <td>${translations["operable"][language]}</td>
-      <td>${totals.operable - operable.length} ${translations["of"][language]} ${totals.operable} </td>
-  </tr>
-  <tr>
-      <td>${translations["understandable"][language]}</td>
-      <td>${totals.understandable - understandable.length} ${translations["of"][language]} ${totals.understandable} </td>
-  </tr>
-  <tr>
-      <td>${translations["robust"][language]}</td>
-      <td>${totals.robust - robust.length} ${translations["of"][language]} ${totals.robust} </td>
-  </tr>
-  <tr>
-      <td>${translations["total"][language]}</td>
-      <td>${totalConforming} ${translations["of"][language]} ${totals.all}</td>
-  </tr>
-  <tbody>
-  </table>`;
+    <tr>
+        <td>${translations["operable"][language]}</td>
+        <td>${totals.operable - operable.length} ${translations["of"][language]} ${totals.operable} </td>
+        <td>${operable.length} ${translations["of"][language]} ${totals.operable} </td>
+    </tr>
+    <tr>
+        <td>${translations["understandable"][language]}</td>
+        <td>${totals.understandable - understandable.length} ${translations["of"][language]} ${totals.understandable} </td>
+        <td>${understandable.length} ${translations["of"][language]} ${totals.understandable} </td>
+    </tr>
+    <tr>
+        <td>${translations["robust"][language]}</td>
+        <td>${totals.robust - robust.length} ${translations["of"][language]} ${totals.robust} </td>
+        <td>${robust.length} ${translations["of"][language]} ${totals.robust} </td>
+    </tr>
+    <tr>
+        <td>${translations["total"][language]}</td>
+        <td>${totalConforming} ${translations["of"][language]} ${totals.all}</td>
+        <td>${totalNotConforming} ${translations["of"][language]} ${totals.all}</td>
+    </tr>
+    <tbody>
+    </table>
+  `;
 }
 
 module.exports = scTable;
